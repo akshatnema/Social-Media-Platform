357984e1e9651494fc4c1a192fe417c8448bd984
akshatnema <20bcs022@iiitdmj.ac.in>
2021-11-24 22:53:41 +0530

    Revert "Merge branch 'main' into Abhinandan"
    This reverts commit 461a8cfb9297be29df9824d7ac1987097e929dba, reversing
    changes made to 9c9e260e06ddd83147ccd5a2e57747548d00ec99.

--- Client side/src/components/Chat-body/chat.jsx
+++ Client side/src/components/Chat-body/chat.jsx
@@ -1,62 +1,13 @@
-import React from "react"
-import "./chat.scss"
-import Send from "./send.jsx"
-import Receive from "./receive.jsx"
+
 
 function chat() {
     return (
-        <div className="chat-body">
-            <div className="d-flex flex-column chat-box">
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Receive message="Hello" />
-                </div>
-                <div className="message-box">
-                    <Send message="Hello" />
-                </div>
-            </div>
-            <div className="type">
-                <i class="far fa-image fa-2x"></i>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="1" placeholder="Type Your message"></textarea>
-            </div>
+        <div class="chat">
+<<<<<<< HEAD
+         <p>  Hello world </p>
+=======
+         
+>>>>>>> 42198130d92198f857c419fd5c613327698217f1
         </div>
     )
 }
--- Client side/src/pages/messenger/messenger.jsx
+++ Client side/src/pages/messenger/messenger.jsx
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+import React,{useContext} from "react";
 import Left from "../../components/Side-bar/left";
 import Navbar from "../../components/Navbar/Navbar";
-import axios from "axios";
 import { AuthContext } from "../../Context/Authcontext";
 import Chatbody from "../../components/Chat-body/chat"
-import Chatlist from "../../components/Chat-body/chatlist"
 import "../../components/Navbar/Navbar.scss";
 import "./messenger.scss";
 import icon from "../../components/Right-Side-bar/pic.jpg";
@@ -12,44 +10,51 @@ import icon from "../../components/Right-Side-bar/pic.jpg";
 function Messenger() {
   const { user } = useContext(AuthContext);
 
-  const Logout = async () => {
-    try {
-      sessionStorage.removeItem("user");
-      document.cookie.split(";").forEach(function (c) {
-        document.cookie = c
-          .replace(/^ +/, "")
-          .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-      });
-      const temp = await axios.get("/auth/logout");
-      console.log(temp);
-      window.location.reload();
-    } catch (err) {}
-  };
-
   return (
     <div className="messenger">
       <Navbar User={user} />
-      <Left Logout={Logout} />
+      <Left />
+<<<<<<< HEAD
+      <div> 
+      <div className="title">
+          <img src={icon} alt="profile-pic" />
+          <div class="title-con">
+            <div className="up"> Akshat</div>
+            <div className="down">last seen</div>
+=======
       <section className="chat">
-        <div className="header">
-          <div className="title">
-            <div className="title-head">
-              <img src={icon} alt="profile-pic" />
-              <div class="title-con">
-                <div className="up"> Akshat</div>
-                <div className="down">last seen</div>
-              </div>
-            </div>
-            <div className="icons">
-              <i class="fas fa-video"></i>
-              <i class="fas fa-phone-alt"></i>
-            </div>
+      <div className="header">  
+      <div className="title">
+          <div className="title-head"> 
+          <img src={icon} alt="profile-pic" />
+           <div class="title-con">
+            <div className="up"> Akshat</div>
+            <div className="down">last seen</div>
+           </div>
+>>>>>>> 42198130d92198f857c419fd5c613327698217f1
           </div>
-          <div className="d-flex justify-content-between side-nav">
-            CHATS
-            <i class="fas fa-search"></i>
+          <div className="icons">
+            <i class="fas fa-video"></i>
+            <i class="fas fa-phone-alt"></i>
           </div>
         </div>
+        <div className="d-flex justify-content-between side-nav">
+          CHATS 
+          <i class="fas fa-search"></i>
+<<<<<<< HEAD
+        </div>
+       <div className="chatbody">
+      <Chatbody />
+      </div>
+      </div>
+=======
+         </div>
+        </div> 
+      <div className="chatbody">
+      <Chatbody />
+      </div>
+      </section>
+>>>>>>> 42198130d92198f857c419fd5c613327698217f1
     </div>
   );
 }
