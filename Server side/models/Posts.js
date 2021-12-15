22ba0e333784e01c5fcecd4161435b42b04a86cd
akshatnema <20bcs022@iiitdmj.ac.in>
2021-12-15 23:55:00 +0530

    updated posts route

--- Server side/models/Posts.js
+++ Server side/models/Posts.js
@@ -21,6 +21,10 @@ const PostSchema = new mongoose.Schema(
       type: Array,
       default: [{}],
     },
+    tags :{
+      type: Array,
+      default: [{}],
+    },
   },
   { timestamps: true }
 );
