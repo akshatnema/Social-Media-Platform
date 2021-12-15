918561ce923bb6c6016fa1f14a694cb8d42397e1
gaur-abhi524 <abhiabhigaur524@gmail.com>
2021-12-15 22:42:53 +0530

    Fix merge conflicts

--- Client side/src/components/Chat-body/chat.jsx
+++ Client side/src/components/Chat-body/chat.jsx
@@ -5,14 +5,6 @@ import Receive from "./receive.jsx"
 
 function chat() {
     return (
-<<<<<<< HEAD
-        <div class="chat">
-<<<<<<< HEAD
-         <p>  Hello world </p>
-=======
-         
->>>>>>> 42198130d92198f857c419fd5c613327698217f1
-=======
         <div className="chat-body">
             <div className="d-flex flex-column chat-box">
                 <div className="message-box">
@@ -65,7 +57,6 @@ function chat() {
                 <i class="far fa-image fa-2x"></i>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="1" placeholder="Type Your message"></textarea>
             </div>
->>>>>>> main
         </div>
     )
 }
